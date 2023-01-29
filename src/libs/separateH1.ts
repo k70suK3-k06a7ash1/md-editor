21bc8f3d65a31508d65100d8c2f260678e866f5e
takayoshi-ishida-lvgs <68541304+nierNieRyorhaYoRHa@users.noreply.github.com>
2023-01-29 12:42:31 +0900

    separeteH1のリファクタリング

--- src/libs/separateH1.ts
+++ src/libs/separateH1.ts
@@ -1,22 +1,18 @@
 export const separateH1 = (content: string) => {
 
   const splitContent = content.split("\n");
-  
-      const separatedSection :string[][]= []
-      let current = 0
-      splitContent.map((row) => {
-      if(/# /.test(row)){
-        current +=1
-        const newRow :string[]= []
-        newRow.push(row)
-        separatedSection.push(newRow)
-      } else {
-        separatedSection[current-1]?.push(row)
-      }
+  const separatedSection :string[][]= []
+  let current = -1
+  splitContent.map((row) => {
+    if(/# /.test(row)){
+      current +=1
+      const newRow :string[]= []
+      newRow.push(row)
+      separatedSection.push(newRow)
+    } else {
+      separatedSection[current]?.push(row)
+    }
   })
-
-console.log(separatedSection)
-
+  console.log(separatedSection)
   return separatedSection
-  
-  }
+}
