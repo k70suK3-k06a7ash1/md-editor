442ce594ade191159fa878ed4cf6ba6a532f5721
2814109 <2814109k@gmail.com>
2023-02-04 11:21:46 +0900

    update card

---
+++ src/components/card/mode/edit.tsx
@@ -0,0 +1,19 @@
+import { FC } from "react";
+import { CardProps } from "../../../types/card/index";
+
+export const EditMode: FC<CardProps> = ({ toggleIsEdit, contentMarkdown }) => {
+  return (
+    <div>
+      <button
+        onClick={() => {
+          toggleIsEdit();
+        }}
+      >
+        Save
+      </button>
+      <div>
+        <textarea>{contentMarkdown}</textarea>
+      </div>
+    </div>
+  );
+};
