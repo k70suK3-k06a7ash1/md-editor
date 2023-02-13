f52e757553361d2db96f3dca42d5b7f6de73746e
2814109 <2814109k@gmail.com>
2023-02-13 23:45:33 +0900

    fix

---
+++ src/libs/splitByTag.ts
@@ -0,0 +1,46 @@
+// type TagLiteral = "h1" | "h2" | "h3" | "h4" | "h5" | "h6";
+//   const splitLiteralToNumber = (splitKey: TagLiteral) => {
+//     switch (splitKey) {
+//       case "h1":
+//         return 1;
+//       case "h2":
+//         return 2;
+
+//       case "h3":
+//         return 3;
+
+//       case "h4":
+//         return 4;
+
+//       case "h5":
+//         return 5;
+
+//       case "h6":
+//         return 6;
+
+//       default: {
+//         splitKey satisfies never;
+//         return 1;
+//       }
+//     }
+//   };
+
+export const splitByTag = (
+  content: string
+  // splitKey: TagLiteral
+) => {
+  const splitCotent = content.split("\n");
+
+  //const numberOfLiteral = splitLiteralToNumber(splitKey);
+  //const pattern = new RegExp(`^\#{${numberOfLiteral}}(?=\s)(.*)$`);
+
+  const pattern = /^\#{1}(?=\s)(.*)$/;
+  const result: string[][] = [];
+  for (let row of splitCotent) {
+    row.match(pattern)
+      ? result?.push([row])
+      : result[result.length - 1]?.push(row);
+  }
+  console.log(result);
+  return result?.map((element) => element.join("\n"));
+};
