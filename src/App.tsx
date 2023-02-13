999e7b21b7672049c773fcbbbef48828021365d1
2814109 <2814109k@gmail.com>
2023-02-14 00:09:30 +0900

    d and d

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,23 @@
 import { useEffect, useState } from "react";
-import { Card } from "./components/card/index";
 import { splitByTag } from "./libs/splitByTag";
-function App() {
-  const [content, setContent] = useState<string[]>();
-  useEffect(() => {
-    import("./base.md").then((response) => {
-      fetch(response.default)
-        .then((response) => response.text())
-        .then((text) => setContent(splitByTag(text, "h1")));
-    });
-  }, []);
+import { useDragComponents } from "./components/dragAndDrop";
 
+function App() {
+  const [contents, setContents] = useState<string[]>([]);
+  // setContents();
+  // useEffect(() => {
+  //   import("./base.md").then((response) => {
+  //     fetch(response.default)
+  //       .then((response) => response.text())
+  //       .then((text) => );
+  //   });
+  // }, []);
+
+  console.log(contents);
+  const { DragAndDropArea } = useDragComponents();
   return (
     <div className="App">
-      {content?.map((element) => (
-        <Card contentMarkdown={element} />
-      ))}
+      <DragAndDropArea />
     </div>
   );
 }
