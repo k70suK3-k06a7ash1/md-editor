4ec24faa691393c1fe6f71172dae657574ac3b90
2814109 <2814109k@gmail.com>
2023-02-13 23:45:33 +0900

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,13 @@
 import { useEffect, useState } from "react";
 import { Card } from "./components/card/index";
-import { separateH1 } from "./libs/separateH1";
-
+import { splitByTag } from "./libs/splitByTag";
 function App() {
-  const [content, setContent] = useState<string[]>([]);
-
+  const [content, setContent] = useState<string[]>();
   useEffect(() => {
     import("./base.md").then((response) => {
       fetch(response.default)
         .then((response) => response.text())
-        .then((text) => setContent(separateH1(text)));
+        .then((text) => setContent(splitByTag(text, "h1")));
     });
   }, []);
 
