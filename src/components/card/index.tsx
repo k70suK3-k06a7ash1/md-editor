67bd4f2cfb08943f2965254f0af0f259db420eec
2814109 <2814109k@gmail.com>
2023-02-17 00:51:42 +0900

    update

---
+++ src/components/card/index.tsx
@@ -0,0 +1,33 @@
+import { Dispatch, FC, useReducer } from "react";
+import { EditMode } from "./mode/edit";
+import { DisplayMode } from "./mode/display";
+import { ContentType } from "../../types/ContentType";
+type Props = {
+  content: ContentType;
+  updateDragList: Dispatch<ContentType>;
+};
+
+export const Card: FC<Props> = ({ content, updateDragList }) => {
+  const [isEdit, toggleIsEdit] = useReducer((state) => {
+    return !state;
+  }, false);
+
+  return (
+    <>
+      {isEdit ? (
+        //   Edit Mode
+        <EditMode
+          updateDragList={updateDragList}
+          toggleIsEdit={toggleIsEdit}
+          content={content}
+        />
+      ) : (
+        // diplay Mode
+        <DisplayMode
+          toggleIsEdit={toggleIsEdit}
+          contentMarkdown={content.content}
+        />
+      )}
+    </>
+  );
+};
--- src/components/card/mode/edit.tsx
+++ src/components/card/mode/edit.tsx
@@ -1,18 +1,35 @@
-import { FC } from "react";
-import { CardProps } from "../../../types/card/index";
+import { Dispatch, DispatchWithoutAction, FC, useState } from "react";
+import { ContentType } from "../../../types/ContentType";
 
-export const EditMode: FC<CardProps> = ({ toggleIsEdit, contentMarkdown }) => {
+type EditCardProps = {
+  content: ContentType;
+  toggleIsEdit: DispatchWithoutAction;
+  updateDragList: Dispatch<ContentType>;
+};
+export const EditMode: FC<EditCardProps> = ({
+  toggleIsEdit,
+  updateDragList,
+  content,
+}) => {
+  const [editContent, setEditContent] = useState<string>(content.content);
+
   return (
     <div>
       <button
         onClick={() => {
+          updateDragList({ id: content.id, content: editContent });
           toggleIsEdit();
         }}
       >
         Save
       </button>
       <div>
-        <textarea defaultValue={contentMarkdown} />
+        <textarea
+          value={editContent}
+          onChange={(event) => {
+            setEditContent(event.target.value);
+          }}
+        />
       </div>
     </div>
   );
