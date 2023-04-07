10c06079c4d91f11a04e3cbd3ea93a6691a90adb
2814109 <2814109k@gmail.com>
2023-04-08 01:15:12 +0900

    set defalut

--- src/recoil/atoms/markdown.ts
+++ src/recoil/atoms/markdown.ts
@@ -1,5 +1,6 @@
 import { atom } from "recoil";
+import { languageAndReadmeMap } from "~/constants/languageAndReadmeMap";
 export const MarkdownState = atom({
   key: "MarkdownState",
-  default: "# ",
+  default: languageAndReadmeMap.english,
 });
