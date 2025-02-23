80603cf91e4da8c71e24951d1bbed3d862f11456
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-02-23 15:31:24 +0900

    chore

--- src/components/atoms/icon/SaveCardIcon/index.tsx
+++ src/components/atoms/icon/SaveCardIcon/index.tsx
@@ -1,27 +1,28 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { DispatchWithoutAction } from "react";
+import type { DispatchWithoutAction } from "react";
 import { IconContainer } from "../../iconContainer";
 import { faFloppyDisk } from "@fortawesome/free-regular-svg-icons";
 import style from "./style.module.css";
 import { Spacer } from "../../Spacer";
 type SaveCardIconProps = {
-  updateContents: DispatchWithoutAction;
-  toggleIsEdit: DispatchWithoutAction;
+	updateContents: DispatchWithoutAction;
+	toggleIsEdit: DispatchWithoutAction;
 };
 export const SaveCardIcon = ({
-  updateContents,
-  toggleIsEdit,
+	updateContents,
+	toggleIsEdit,
 }: SaveCardIconProps): JSX.Element => (
-  <IconContainer>
-    <i
-      onClick={() => {
-        updateContents();
-        toggleIsEdit();
-      }}
-    >
-      <FontAwesomeIcon className={style.saveCardIcon} icon={faFloppyDisk} />
-      <Spacer horizontal size={8} />
-      Save
-    </i>
-  </IconContainer>
+	<IconContainer>
+		{/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+		<i
+			onClick={() => {
+				updateContents();
+				toggleIsEdit();
+			}}
+		>
+			<FontAwesomeIcon className={style.saveCardIcon} icon={faFloppyDisk} />
+			<Spacer horizontal size={8} />
+			Save
+		</i>
+	</IconContainer>
 );
