922bbff8d96badd3933e024dd06adf22da9a08f6
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-02-23 15:45:52 +0900

    chore

--- src/recoil/atoms/markdown.ts
+++
@@ -1,6 +0,0 @@
-import { atom } from "recoil";
-import { languageAndReadmeMap } from "~/constants/languageAndReadmeMap";
-export const MarkdownState = atom({
-  key: "MarkdownState",
-  default: languageAndReadmeMap.english,
-});
--- src/recoil/atoms/separateLevel.ts
+++
@@ -1,7 +0,0 @@
-import { atom } from "recoil";
-
-export type SeparateLevelType = "h1" | "h2" | "h3";
-export const SeparateLevel = atom<SeparateLevelType>({
-  key: "SeparateLevel",
-  default: "h2",
-});
