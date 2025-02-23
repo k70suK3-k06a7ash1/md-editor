78f521cc6025650f0a614312a5aedd0e915dfcef
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-02-23 15:33:00 +0900

    chore

--- src/components/features/card/mode/display/index.tsx
+++ src/components/features/card/mode/display/index.tsx
@@ -1,23 +1,25 @@
-import { DispatchWithoutAction, FC } from "react";
+import type { DispatchWithoutAction } from "react";
 import ReactMarkdown from "react-markdown";
 import { EditCardIcon } from "~/components/atoms/icon/EditCardIcon";
 import remarkGfm from "remark-gfm";
 import style from "./style.module.css";
 
 type DisplayCardProps = {
-  toggleIsEdit: DispatchWithoutAction;
-  contentMarkdown: string;
+	toggleIsEdit: DispatchWithoutAction;
+	contentMarkdown: string;
 };
-export const DisplayMode: FC<DisplayCardProps> = ({
-  toggleIsEdit,
-  contentMarkdown,
-}) => {
-  return (
-    <div className={`${style.wrapper} markdown-body ${style.filterInvert}`}>
-      <div className={style.iconPosition}>
-        <EditCardIcon toggleIsEdit={toggleIsEdit} />
-      </div>
-      <ReactMarkdown children={contentMarkdown} remarkPlugins={[remarkGfm]} />
-    </div>
-  );
+export const DisplayMode = ({
+	toggleIsEdit,
+	contentMarkdown,
+}: DisplayCardProps) => {
+	return (
+		<div className={`${style.wrapper} markdown-body ${style.filterInvert}`}>
+			<div className={style.iconPosition}>
+				<EditCardIcon toggleIsEdit={toggleIsEdit} />
+			</div>
+			<ReactMarkdown remarkPlugins={[remarkGfm]}>
+				{contentMarkdown}
+			</ReactMarkdown>
+		</div>
+	);
 };
