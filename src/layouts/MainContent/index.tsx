902ea2c17021c2dc8282a04216e3156e8a65c0bb
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-02-23 15:36:17 +0900

    chore

--- src/layouts/MainContent/index.tsx
+++ src/layouts/MainContent/index.tsx
@@ -1,8 +1,6 @@
-import React, { ReactNode } from "react";
+import type { PropsWithChildren } from "react";
 import style from "./style.module.css";
-type Props = {
-	children: ReactNode;
-};
-export const MainContent = ({ children }: Props) => (
+
+export const MainContent = ({ children }: PropsWithChildren) => (
 	<div className={style.mainContainer}>{children}</div>
 );
