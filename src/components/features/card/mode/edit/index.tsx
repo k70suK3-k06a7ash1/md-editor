aaf5741abaf0e88f4c7ff32ee1c31fb6cba89358
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-02-23 15:33:40 +0900

    chore

--- src/components/features/card/mode/edit/index.tsx
+++ src/components/features/card/mode/edit/index.tsx
@@ -1,43 +1,43 @@
-import { Dispatch, DispatchWithoutAction, FC, useState } from "react";
-import { ContentType } from "~/types";
+import { type Dispatch, type DispatchWithoutAction, useState } from "react";
+import type { ContentType } from "~/types";
 import style from "./style.module.css";
 import { SaveCardIcon } from "~/components/atoms/icon/SaveCardIcon";
 import { useIsIncludeTag } from "~/hooks/useIsIncludeTag";
 
 type EditCardProps = {
-  content: ContentType;
-  toggleIsEdit: DispatchWithoutAction;
-  updateContents: Dispatch<ContentType>;
+	content: ContentType;
+	toggleIsEdit: DispatchWithoutAction;
+	updateContents: Dispatch<ContentType>;
 };
 
-export const EditMode: FC<EditCardProps> = ({
-  toggleIsEdit,
-  updateContents,
-  content,
-}) => {
-  const { isIncludeTag } = useIsIncludeTag();
-  const [editContent, setEditContent] = useState<string>(content.content);
+export const EditMode = ({
+	toggleIsEdit,
+	updateContents,
+	content,
+}: EditCardProps) => {
+	const { isIncludeTag } = useIsIncludeTag();
+	const [editContent, setEditContent] = useState<string>(content.content);
 
-  const handleUpdateContents = () => {
-    isIncludeTag(editContent.split("\n")[0])
-      ? updateContents({ id: content.id, content: editContent })
-      : window.alert("The first line must have one to two # symbols");
-  };
-  return (
-    <div className={style.wrapper}>
-      <div className={style.iconPosition}>
-        <SaveCardIcon
-          updateContents={handleUpdateContents}
-          toggleIsEdit={toggleIsEdit}
-        />
-      </div>
-      <textarea
-        className={style.textArea}
-        value={editContent}
-        onChange={(event) => {
-          setEditContent(event.target.value);
-        }}
-      />
-    </div>
-  );
+	const handleUpdateContents = () => {
+		isIncludeTag(editContent.split("\n")[0])
+			? updateContents({ id: content.id, content: editContent })
+			: window.alert("The first line must have one to two # symbols");
+	};
+	return (
+		<div className={style.wrapper}>
+			<div className={style.iconPosition}>
+				<SaveCardIcon
+					updateContents={handleUpdateContents}
+					toggleIsEdit={toggleIsEdit}
+				/>
+			</div>
+			<textarea
+				className={style.textArea}
+				value={editContent}
+				onChange={(event) => {
+					setEditContent(event.target.value);
+				}}
+			/>
+		</div>
+	);
 };
--- src/components/features/language/index.tsx
+++ src/components/features/language/index.tsx
@@ -1,25 +1,25 @@
-import { ChangeEvent, Dispatch } from "react";
-import { LanguageKey } from "~/types/figurative/LanguageType";
+import type { ChangeEvent, Dispatch } from "react";
+import type { LanguageKey } from "~/types/figurative/LanguageType";
 import style from "./style.module.css";
 
 type LanguageSelectedProps = {
-  defaultKey: LanguageKey;
-  handleChangeLanguage: Dispatch<LanguageKey>;
+	defaultKey: LanguageKey;
+	handleChangeLanguage: Dispatch<LanguageKey>;
 };
 export const LanguageSelected = ({
-  defaultKey,
-  handleChangeLanguage,
+	defaultKey,
+	handleChangeLanguage,
 }: LanguageSelectedProps): JSX.Element => (
-  <div className={style.languageSelector}>
-    <select
-      defaultValue={defaultKey}
-      onChange={(event: ChangeEvent<HTMLSelectElement>) => {
-        handleChangeLanguage(event.currentTarget.value as LanguageKey);
-      }}
-    >
-      <option value={"english"}>English</option>
-      <option value={"japanese"}>Japanese</option>
-      <option value={"chinese"}>Chinese</option>
-    </select>
-  </div>
+	<div className={style.languageSelector}>
+		<select
+			defaultValue={defaultKey}
+			onChange={(event: ChangeEvent<HTMLSelectElement>) => {
+				handleChangeLanguage(event.currentTarget.value as LanguageKey);
+			}}
+		>
+			<option value={"english"}>English</option>
+			<option value={"japanese"}>Japanese</option>
+			<option value={"chinese"}>Chinese</option>
+		</select>
+	</div>
 );
