90f2fa6dad7d7c87c4b862f4c0049a1242824e08
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-02-23 15:31:10 +0900

    chore

--- src/components/atoms/icon/EditCardIcon/index.tsx
+++ src/components/atoms/icon/EditCardIcon/index.tsx
@@ -1,24 +1,25 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { DispatchWithoutAction } from "react";
+import type { DispatchWithoutAction } from "react";
 import { IconContainer } from "../../iconContainer";
 import { faPenToSquare } from "@fortawesome/free-regular-svg-icons";
 import style from "./style.module.css";
 import { Spacer } from "../../Spacer";
 type EditCardIconProps = {
-  toggleIsEdit: DispatchWithoutAction;
+	toggleIsEdit: DispatchWithoutAction;
 };
 export const EditCardIcon = ({
-  toggleIsEdit,
+	toggleIsEdit,
 }: EditCardIconProps): JSX.Element => (
-  <IconContainer>
-    <i
-      onClick={() => {
-        toggleIsEdit();
-      }}
-    >
-      <FontAwesomeIcon className={style.icon} icon={faPenToSquare} />
-      <Spacer horizontal size={8} />
-      Edit
-    </i>
-  </IconContainer>
+	<IconContainer>
+		{/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+		<i
+			onClick={() => {
+				toggleIsEdit();
+			}}
+		>
+			<FontAwesomeIcon className={style.icon} icon={faPenToSquare} />
+			<Spacer horizontal size={8} />
+			Edit
+		</i>
+	</IconContainer>
 );
--- src/components/atoms/icon/FileImportIcon/index.tsx
+++ src/components/atoms/icon/FileImportIcon/index.tsx
@@ -1,21 +1,21 @@
-import { FC, Dispatch, ChangeEvent } from "react";
+import type { Dispatch, ChangeEvent } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faFileImport } from "@fortawesome/free-solid-svg-icons";
 import style from "./style.module.css";
 import { Spacer } from "../../Spacer";
 import { TabItem } from "../../../../styles/tabItem";
 type Props = {
-  handleClick: Dispatch<ChangeEvent<HTMLInputElement>>;
+	handleClick: Dispatch<ChangeEvent<HTMLInputElement>>;
 };
-export const FileImport: FC<Props> = ({ handleClick }) => (
-  <TabItem>
-    <label className={style.container}>
-      <i>
-        <FontAwesomeIcon className={style.icon} icon={faFileImport} />
-      </i>
-      <Spacer horizontal size={4} />
-      <div>Import</div>
-      <input type="file" accept=".md" onChange={handleClick} />
-    </label>
-  </TabItem>
+export const FileImport = ({ handleClick }: Props) => (
+	<TabItem>
+		<label className={style.container}>
+			<i>
+				<FontAwesomeIcon className={style.icon} icon={faFileImport} />
+			</i>
+			<Spacer horizontal size={4} />
+			<div>Import</div>
+			<input type="file" accept=".md" onChange={handleClick} />
+		</label>
+	</TabItem>
 );
