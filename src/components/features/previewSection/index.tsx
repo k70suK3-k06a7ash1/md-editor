c149d120add7061b2f97597cef735b87952abc71
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-02-23 15:34:08 +0900

    chore

--- src/components/features/previewSection/index.tsx
+++ src/components/features/previewSection/index.tsx
@@ -1,24 +1,22 @@
 import style from "./style.module.css";
-import { ContentType } from "~/types";
+import type { ContentType } from "~/types";
 import ReactMarkdown from "react-markdown";
 
 import remarkGfm from "remark-gfm";
-import { Spacer } from "~/components/atoms/Spacer";
 
 type PreviewSectionProps = {
-  contents: ContentType[];
+	contents: ContentType[];
 };
 export const PreviewSection = ({
-  contents,
+	contents,
 }: PreviewSectionProps): JSX.Element => (
-  <>
-    {contents?.length > 0 && (
-      <div className={`markdown-body ${style.previewContent}`}>
-        <ReactMarkdown
-          children={contents.map(({ content }) => content).join("\n")}
-          remarkPlugins={[remarkGfm]}
-        />
-      </div>
-    )}
-  </>
+	<>
+		{contents?.length > 0 && (
+			<div className={`markdown-body ${style.previewContent}`}>
+				<ReactMarkdown remarkPlugins={[remarkGfm]}>
+					{contents.map(({ content }) => content).join("\n")}
+				</ReactMarkdown>
+			</div>
+		)}
+	</>
 );
