8607e55c670a5e8069520c1865c7c2a73b826a57
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-02-23 15:34:52 +0900

    chore

--- src/components/features/selectSeparateLevel/index.tsx
+++ src/components/features/selectSeparateLevel/index.tsx
@@ -1,32 +1,35 @@
 import { faArrowLeft, faArrowRight } from "@fortawesome/free-solid-svg-icons";
 import { H1Icon, H2Icon, H3Icon } from "./level";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { FC } from "react";
 import style from "./index.module.css";
 import { useSeparateLevel } from "~/hooks/useSeparateLevel";
-export const SelectSeparateLevel: FC = () => {
-  const { separateLevel, onClickByLevel, onClickDecrement, onClickIncrement } =
-    useSeparateLevel();
+export const SelectSeparateLevel = () => {
+	const { separateLevel, onClickByLevel, onClickDecrement, onClickIncrement } =
+		useSeparateLevel();
 
-  return (
-    <div className={style.wrapper}>
-      <span>Current Level: {separateLevel}</span>
-      <i onClick={() => onClickDecrement()}>
-        <FontAwesomeIcon className={style.icon} icon={faArrowLeft} />
-      </i>
-      <i onClick={() => onClickByLevel("h1")}>
-        <H1Icon />
-      </i>
-      <i onClick={() => onClickByLevel("h2")}>
-        <H2Icon />
-      </i>
-      <i onClick={() => onClickByLevel("h3")}>
-        <H3Icon />
-      </i>
-
-      <i onClick={() => onClickIncrement()}>
-        <FontAwesomeIcon className={style.icon} icon={faArrowRight} />
-      </i>
-    </div>
-  );
+	return (
+		<div className={style.wrapper}>
+			<span>Current Level: {separateLevel}</span>
+			{/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+			<i onClick={() => onClickDecrement()}>
+				<FontAwesomeIcon className={style.icon} icon={faArrowLeft} />
+			</i>
+			{/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+			<i onClick={() => onClickByLevel("h1")}>
+				<H1Icon />
+			</i>
+			{/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+			<i onClick={() => onClickByLevel("h2")}>
+				<H2Icon />
+			</i>
+			{/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+			<i onClick={() => onClickByLevel("h3")}>
+				<H3Icon />
+			</i>{" "}
+			{/* biome-ignore lint/a11y/useKeyWithClickEvents: <explanation> */}
+			<i onClick={() => onClickIncrement()}>
+				<FontAwesomeIcon className={style.icon} icon={faArrowRight} />
+			</i>
+		</div>
+	);
 };
