b94bb94f51265120d8a1d2b5c90cf073064b92e2
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-02-23 14:30:29 +0900

    chore

--- src/components/atoms/icon/CancelIcon/index.tsx
+++ src/components/atoms/icon/CancelIcon/index.tsx
@@ -1,21 +1,22 @@
-import { DispatchWithoutAction, FC } from "react";
-import { faArrowTurnRight } from "@fortawesome/free-solid-svg-icons";
+import type { DispatchWithoutAction, FC } from "react";
+import { faArrowsTurnRight } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import style from "./style.module.css";
 import { Spacer } from "../../Spacer";
 import { IncludeIconButton } from "~/styles/IncludeIconButton";
 type Props = {
-  handleCancel: DispatchWithoutAction;
+	handleCancel: DispatchWithoutAction;
 };
 export const CancelIcon: FC<Props> = ({ handleCancel }) => (
-  <IncludeIconButton>
-    <label className={style.container}>
-      <i>
-        <FontAwesomeIcon className={style.icon} icon={faArrowTurnRight} />
-      </i>
-      <Spacer horizontal size={4} />
-      <div>Cancel</div>
-      <button onClick={handleCancel} type="button" />
-    </label>
-  </IncludeIconButton>
+	<IncludeIconButton>
+		{/* biome-ignore lint/a11y/noLabelWithoutControl: <explanation> */}
+		<label className={style.container}>
+			<i>
+				<FontAwesomeIcon className={style.icon} icon={faArrowsTurnRight} />
+			</i>
+			<Spacer horizontal size={4} />
+			<div>Cancel</div>
+			<button onClick={handleCancel} type="button" />
+		</label>
+	</IncludeIconButton>
 );
