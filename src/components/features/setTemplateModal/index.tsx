12de456e30a72b90958521d277a7a7aff41f3ac8
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-02-23 15:35:29 +0900

    chore

--- src/components/features/setTemplateModal/index.tsx
+++ src/components/features/setTemplateModal/index.tsx
@@ -1,45 +1,45 @@
-import { Dispatch, FC, useState } from "react";
-import { DispatchWithoutAction } from "react";
+import { type Dispatch, type DispatchWithoutAction, useState } from "react";
+import {} from "react";
 import { ApplyTemplateIcon } from "~/components/atoms/icon/ApplyTemplateIcon";
 import { CancelIcon } from "~/components/atoms/icon/CancelIcon";
 import { Spacer } from "~/components/atoms/Spacer";
-import { LanguageKey } from "~/types/figurative/LanguageType";
+import type { LanguageKey } from "~/types/figurative/LanguageType";
 import { LanguageSelected } from "../language";
 import style from "./style.module.css";
 
 type Props = {
-  handleClose: DispatchWithoutAction;
-  handleApply: Dispatch<LanguageKey>;
+	handleClose: DispatchWithoutAction;
+	handleApply: Dispatch<LanguageKey>;
 };
-export const SetTemplateModal: FC<Props> = ({ handleClose, handleApply }) => {
-  const [languageKey, setLanguageKey] = useState<LanguageKey>("english");
+export const SetTemplateModal = ({ handleClose, handleApply }: Props) => {
+	const [languageKey, setLanguageKey] = useState<LanguageKey>("english");
 
-  const handleOnClickApplyTemplate = () => {
-    handleApply(languageKey);
-    handleClose();
-  };
+	const handleOnClickApplyTemplate = () => {
+		handleApply(languageKey);
+		handleClose();
+	};
 
-  return (
-    <div className={style.modalContainer}>
-      <div className={style.modalSelectSection}>
-        <h1>Please select a language.</h1>
-      </div>
-      <Spacer size={24} />
+	return (
+		<div className={style.modalContainer}>
+			<div className={style.modalSelectSection}>
+				<h1>Please select a language.</h1>
+			</div>
+			<Spacer size={24} />
 
-      <div className={style.modalSelectSection}>
-        <div>
-          <LanguageSelected
-            defaultKey={languageKey}
-            handleChangeLanguage={setLanguageKey}
-          />
-        </div>
-      </div>
+			<div className={style.modalSelectSection}>
+				<div>
+					<LanguageSelected
+						defaultKey={languageKey}
+						handleChangeLanguage={setLanguageKey}
+					/>
+				</div>
+			</div>
 
-      <Spacer size={48} />
-      <div className={style.modalBottomButtonSection}>
-        <CancelIcon handleCancel={handleClose} />
-        <ApplyTemplateIcon handleApply={handleOnClickApplyTemplate} />
-      </div>
-    </div>
-  );
+			<Spacer size={48} />
+			<div className={style.modalBottomButtonSection}>
+				<CancelIcon handleCancel={handleClose} />
+				<ApplyTemplateIcon handleApply={handleOnClickApplyTemplate} />
+			</div>
+		</div>
+	);
 };
