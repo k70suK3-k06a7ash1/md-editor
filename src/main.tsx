5c5e5865f2999e9177ea0f60ad635dd25466d538
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-02-24 19:32:48 +0900

    chore

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,13 @@
-import React from "react";
+import { StrictMode } from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 import { MarkdownProvider } from "@/context/MarkdownContext";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-	<React.StrictMode>
+	<StrictMode>
 		<MarkdownProvider>
 			<App />
 		</MarkdownProvider>
-	</React.StrictMode>,
+	</StrictMode>,
 );
