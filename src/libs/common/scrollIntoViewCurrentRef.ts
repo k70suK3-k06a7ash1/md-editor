9f02d7bc3434860c1a3e1449c7ab03ee509c63ad
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-02-24 20:22:50 +0900

    chore

--- src/libs/common/scrollIntoViewCurrentRef.ts
+++ src/libs/common/scrollIntoViewCurrentRef.ts
@@ -1,9 +1,9 @@
-import { RefObject } from "react";
+import type { RefObject } from "react";
 
 export const scrollIntoViewCurrentRef = (
-  currentRef: RefObject<HTMLDivElement>
+	currentRef: RefObject<HTMLDivElement>,
 ) =>
-  currentRef?.current?.scrollIntoView({
-    behavior: "smooth",
-    block: "start",
-  });
+	currentRef?.current?.scrollIntoView({
+		behavior: "smooth",
+		block: "start",
+	});
