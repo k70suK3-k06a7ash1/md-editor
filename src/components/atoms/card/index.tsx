f598da8c825fe231c64628374074bb1e4e0185c2
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-02-24 20:28:31 +0900

    chore

--- src/components/atoms/card/index.tsx
+++
@@ -1,7 +0,0 @@
-import type { PropsWithChildren } from "react";
-
-export const Card = ({ children }: PropsWithChildren) => (
-	<div className="rounded-lg border border-gray-300 bg-white w-full overflow-hidden shadow-sm">
-		{children}
-	</div>
-);
