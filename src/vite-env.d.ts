af7edc706df7acf0b31d967a3a8225e01b985796
k70suK3-k06a7ash1 <2814109k@gmail.com>
2025-02-24 19:39:25 +0900

    chore

---
+++ src/vite-env.d.ts
@@ -0,0 +1,10 @@
+/// <reference types="vite/client" />
+
+// interface ImportMetaEnv {
+// 	readonly VITE_APP_GITHUB_TOKEN: string;
+// 	readonly VITE_APP_USER_NAME: string;
+// }
+
+// interface ImportMeta {
+// 	readonly env: ImportMetaEnv;
+// }
